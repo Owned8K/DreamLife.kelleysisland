9b811b5eb6e2bec8474b6311c3ec5d5ee9116baa
ASUSPC\Utilisateur <owned8k@gmail.com>
2025-07-20 23:36:26 +0200

    fix message button

--- dialog/smartphone.hpp
+++ dialog/smartphone.hpp
@@ -58,28 +58,19 @@ class Life_Smartphone {
 
         class MessageButton: Life_RscButtonMenu {
             idc = 88806;
-            type = 16;
-            style = "0x02 + 0xC0";
             x = (0.6 + 0.24) * safezoneW + safezoneX;
             y = (0.418 + 0.42) * safezoneH + safezoneY;
             w = 0.035 * safezoneW;
             h = 0.035 * safezoneH;
-            colorBackground[] = {0,0,0,0};
+            colorBackground[] = {1,1,1,1};
             colorBackgroundFocused[] = {0,0,0,0};
             colorBackground2[] = {0,0,0,0};
             colorFocused[] = {0,0,0,0};
             colorText[] = {0,0,0,0};
             colorDisabled[] = {0,0,0,0};
             colorBackgroundDisabled[] = {0,0,0,0};
-            onMouseEnter = "diag_log 'Message Button: Mouse Enter'";
-            onMouseExit = "diag_log 'Message Button: Mouse Exit'";
-            onButtonClick = "diag_log 'Message Button: Clicked'; hint 'Application Messages'";
+            onButtonClick = "hint 'Application Messages'";
             tooltip = "Messages";
-            default = 0;
-            text = "";
-            shadow = 0;
-            font = "RobotoCondensed";
-            sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
         };
 
         class MainContent: Life_RscControlsGroup {
