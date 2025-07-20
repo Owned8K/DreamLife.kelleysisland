2c7c6ff5c26616f09d3edbfc8d9f3e165cfed771
ASUSPC\Utilisateur <owned8k@gmail.com>
2025-07-20 22:55:24 +0200

    Fix

--- dialog/smartphone.hpp
+++ dialog/smartphone.hpp
@@ -58,6 +58,7 @@ class Life_Smartphone {
 
         class MessageButton: Life_RscButtonMenu {
             idc = 88806;
+            style = 2;
             x = (0.6 + 0.22) * safezoneW + safezoneX;
             y = (0.418 + 0.42) * safezoneH + safezoneY;
             w = 0.035 * safezoneW;
@@ -71,6 +72,11 @@ class Life_Smartphone {
             colorBackgroundDisabled[] = {0,0,0,0};
             onButtonClick = "hint 'Application Messages'";
             tooltip = "Messages";
+            offsetX = 0;
+            offsetY = 0;
+            offsetPressedX = 0;
+            offsetPressedY = 0;
+            borderSize = 0;
         };
 
         class MainContent: Life_RscControlsGroup {
