ecd07e2d8f2052adad856f627f92b148c82e167a
Anthony <owned8k@gmail.com>
2025-07-20 10:03:20 +0200

    On ajuste

--- dialog/smartphone.hpp
+++ dialog/smartphone.hpp
@@ -12,18 +12,18 @@ class Life_Smartphone {
         class PhoneBackground: life_RscPicture {
             idc = -1;
             text = "textures\phone\phone.paa";
-            x = 0.3 * safezoneW + safezoneX;
-            y = 0.159 * safezoneH + safezoneY;
-            w = 0.4 * safezoneW;
+            x = 0.6 * safezoneW + safezoneX;
+            y = 0.318 * safezoneH + safezoneY;
+            w = 0.45 * safezoneW;
             h = 0.682 * safezoneH;
             colorText[] = {1,1,1,1};
         };
 
         class MainContent: Life_RscControlsGroup {
             idc = 88802;
-            x = 0.32 * safezoneW + safezoneX;
-            y = 0.225 * safezoneH + safezoneY;
-            w = 0.36 * safezoneW;
+            x = 0.62 * safezoneW + safezoneX;
+            y = 0.384 * safezoneH + safezoneY;
+            w = 0.41 * safezoneW;
             h = 0.55 * safezoneH;
             
             class Controls {
