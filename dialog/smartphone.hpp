1b371cfeb3af672614a7ed8511530626fc496b93
Anthony <owned8k@gmail.com>
2025-07-20 10:20:09 +0200

    Fix icones

--- dialog/smartphone.hpp
+++ dialog/smartphone.hpp
@@ -27,46 +27,54 @@ class Life_Smartphone {
             h = 0.45 * safezoneH;
             
             class Controls {
-                class PhoneAppButton: Life_RscButtonInvisible {
+                class PhoneIcon: life_RscPicture {
                     idc = 88803;
+                    text = "textures\phone\telephone.paa";
                     x = 0.01 * safezoneW;
                     y = 0.01 * safezoneH;
                     w = 0.08 * safezoneW;
                     h = 0.08 * safezoneH;
-                    onButtonClick = "hint 'Application Téléphone'";
-                    tooltip = "Téléphone";
-                    class Controls {
-                        class PhoneIcon: life_RscPicture {
-                            idc = -1;
-                            text = "textures\phone\telephone.paa";
-                            x = 0;
-                            y = 0;
-                            w = 0.08 * safezoneW;
-                            h = 0.08 * safezoneH;
-                            colorText[] = {1,1,1,1};
-                        };
-                    };
+                    colorText[] = {1,1,1,1};
                 };
 
-                class MessagesAppButton: Life_RscButtonInvisible {
+                class PhoneButton: Life_RscButtonMenu {
                     idc = 88804;
+                    x = 0.01 * safezoneW;
+                    y = 0.01 * safezoneH;
+                    w = 0.08 * safezoneW;
+                    h = 0.08 * safezoneH;
+                    colorBackground[] = {0,0,0,0};
+                    colorBackgroundFocused[] = {0,0,0,0};
+                    colorBackground2[] = {0,0,0,0};
+                    colorFocused[] = {0,0,0,0};
+                    colorText[] = {0,0,0,0};
+                    onButtonClick = "hint 'Application Téléphone'";
+                    tooltip = "Téléphone";
+                };
+
+                class MessageIcon: life_RscPicture {
+                    idc = 88805;
+                    text = "textures\phone\messages.paa";
                     x = 0.11 * safezoneW;
                     y = 0.01 * safezoneH;
                     w = 0.08 * safezoneW;
                     h = 0.08 * safezoneH;
+                    colorText[] = {1,1,1,1};
+                };
+
+                class MessageButton: Life_RscButtonMenu {
+                    idc = 88806;
+                    x = 0.11 * safezoneW;
+                    y = 0.01 * safezoneH;
+                    w = 0.08 * safezoneW;
+                    h = 0.08 * safezoneH;
+                    colorBackground[] = {0,0,0,0};
+                    colorBackgroundFocused[] = {0,0,0,0};
+                    colorBackground2[] = {0,0,0,0};
+                    colorFocused[] = {0,0,0,0};
+                    colorText[] = {0,0,0,0};
                     onButtonClick = "hint 'Application Messages'";
                     tooltip = "Messages";
-                    class Controls {
-                        class MessageIcon: life_RscPicture {
-                            idc = -1;
-                            text = "textures\phone\messages.paa";
-                            x = 0;
-                            y = 0;
-                            w = 0.08 * safezoneW;
-                            h = 0.08 * safezoneH;
-                            colorText[] = {1,1,1,1};
-                        };
-                    };
                 };
             };
         };
