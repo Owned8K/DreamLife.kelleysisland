8a46a84b67aaa80aa33f5f29a8e4acdea4797115
ASUSPC\Utilisateur <owned8k@gmail.com>
2025-07-21 10:31:57 +0200

    Essayons une autre approche en utilisant le caractère ASCII 160

--- dialog/smartphone.hpp
+++ dialog/smartphone.hpp
@@ -71,7 +71,8 @@ class Life_Smartphone {
             colorScrollbar[] = {0.95,0.95,0.95,1};
             sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.7)";
             rowHeight = 0.08;
-            style = 16 + 0x10;
+            style = 16 + 0x10 + 0x200;
+            lineSpacing = 1;
             show = 0;
         };
 
@@ -88,7 +89,7 @@ class Life_Smartphone {
             colorText[] = {0,0,0,0};
             colorDisabled[] = {0,0,0,0};
             colorBackgroundDisabled[] = {0,0,0,0};
-            onButtonClick = "ctrlShow [88803, false]; ctrlShow [88804, false]; ctrlShow [88805, false]; ctrlShow [88806, false]; ctrlShow [88807, true]; ctrlShow [88808, true]; _display = findDisplay 88800; _listBox = _display displayCtrl 88808; lbClear _listBox; _newline = toString [13,10]; _listBox lbAdd format['MESSAGE DE JOHN DOE%1   Salut, j''ai une mission pour toi...', _newline]; _listBox lbAdd format['MESSAGE DE JANE SMITH%1   As-tu les documents demandés ?', _newline]; _listBox lbAdd format['MESSAGE DE BOB WILSON%1   RDV au garage dans 10min', _newline]; _listBox lbAdd format['MESSAGE DU MAIRE%1   Réunion importante à la mairie...', _newline];";
+            onButtonClick = "ctrlShow [88803, false]; ctrlShow [88804, false]; ctrlShow [88805, false]; ctrlShow [88806, false]; ctrlShow [88807, true]; ctrlShow [88808, true]; _display = findDisplay 88800; _listBox = _display displayCtrl 88808; lbClear _listBox; _space = toString [160,160,160,160,160]; _listBox lbAdd format['MESSAGE DE JOHN DOE%1Salut, j''ai une mission pour toi...', _space]; _listBox lbAdd format['MESSAGE DE JANE SMITH%1As-tu les documents demandés ?', _space]; _listBox lbAdd format['MESSAGE DE BOB WILSON%1RDV au garage dans 10min', _space]; _listBox lbAdd format['MESSAGE DU MAIRE%1Réunion importante à la mairie...', _space];";
             tooltip = "Messages";
         };
 
